7af337aec1e07af1778b541055cc6f772f262918
Lauris2007 <lauramarro2018@gmail.com>
2026-03-05 22:44:48 -0500

    Vista en React

---
+++ Git_proyecto/Proyecto_FE/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Editar_perfil from "./paginas/Editar_perfil";
+
+function App() {
+  return <Editar_perfil />;
+}
+export default App;
